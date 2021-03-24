acee9dd2ffd60521916a351cb50e8230c0df00ae
ozt-ca <ozt.dei@gmail.com>
2021-03-24 14:00:09 +0900

    Added some functions

---
+++ R/analysis.R
@@ -0,0 +1,91 @@
+#' Fit a model
+#'
+#' @return
+#'
+#' @export
+#'
+#' @examples
+#'
+nnmrktFit <- function(){
+  print("This is an April Fool joke!")
+}
+
+#' Predict future values
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nnmrktPredict <- function(){
+  print("This is an April Fool joke!")
+}
+
+#' Preprocess a table dataset
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nnmrktPreprocessTableData <- function(){
+  print("This is an April Fool joke!")
+}
+
+#' Preprocess a time series dataset
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nnmrktPreprocessTimeSeriesData <- function(){
+  print("This is an April Fool joke!")
+}
+
+#' Preprocess an image dataset
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nnmrktPreprocessImageData <- function(){
+  print("This is an April Fool joke!")
+}
+
+#' Preprocess a text dataset
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nnmrktPreprocessTextData <- function(){
+  print("This is an April Fool joke!")
+}
+
+#' Preprocess an audio dataset
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nnmrktPreprocessAudioData <- function(){
+  print("This is an April Fool joke!")
+}
+
+#' Preprocess a movie dataset
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nnmrktPreprocessMovieData <- function(){
+  print("This is an April Fool joke!")
+}
+
+#' Preprocess a recommendation dataset
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nnmrktPreprocessRecommendData <- function(){
+  print("This is an April Fool joke!")
+}
---
+++ R/outputs.R
@@ -0,0 +1,29 @@
+#' Arrange plots of outputs
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ArrangePlots <- function(){
+  print("This is an April Fool joke!")
+}
+
+#' Format outputs
+#'
+#' @return
+#' @export
+#'
+#' @examples
+FormatOuputs <- function(){
+  print("This is an April Fool joke!")
+}
+
+#' Summarize nnmrkt's outputs
+#'
+#' @return
+#' @export
+#'
+#' @examples
+SummarizeOutputs <- function(){
+  print("This is an April Fool joke!")
+}
